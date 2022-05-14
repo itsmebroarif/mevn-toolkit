083a96a950b4377773ec03a7031afca5bd95d00e
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2022-05-15 04:28:39 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="aboutMe">
-      <h1>This is an about page</h1> 
+      <h1>About Page</h1> 
     <p>
 Hi There, My name is arif and i'm the Mentor & Admin of Code With Arif, so in this website i'll describe who am i and what is this. so, let me introduce myself first, i'm a mentor who makes the free class for everyone and today, i've made this website for the people who want to learn how to make a website with vue js 3 and connection js.
     </p>
